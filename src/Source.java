e56c7d0ddee08dea7cd5e5817966f5e954df532a
AliaksandrKryvapust <alexandrkryvopust@gmail.com>
2022-09-02 10:21:21 +0200

    Final

---
+++ src/Source.java
@@ -0,0 +1,17 @@
+public class Source {
+    public static void main(String[] args) {
+    String str = "+";
+    int a = 4;
+    int b =7;
+        System.out.println(basicMath(str,a,b)==11);
+    }
+    public static Integer basicMath(String op, int v1, int v2)    {
+        return switch (op) {
+            case "+" -> v1 + v2;
+            case "-" -> v1 - v2;
+            case "*" -> v1 * v2;
+            case "/" -> v1 / v2;
+            default -> 0;
+        };
+    }
+}
